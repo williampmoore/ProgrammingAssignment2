1f6f2e9daf38c0fb89f633de9b8356088a4b7cad
William Moore <william.moore@360i.com>
2015-07-22 14:13:42 +0100

    Do assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Make an inverse caching matrix
 
-## Write a short comment describing this function
+## Create the cacheMatrix structure/class with get, set, getInverse
+##  and setInverse methods
+makeCacheMatrix <- function(inputMatrix = matrix())
+{
+  # Validate inputMatrix is a square matrix
+  if(!ValidateMatrix(inputMatrix)) return("Error: Input not a square matrix");
+  
+  matrixInverse <- NULL
+  
+  # set the inputMatrix and set inverse to NULL
+  set <- function(mtx)
+  {
+    inputMatrix <<- mtx
+    matrixInverse <<- NULL
+  }
+  
+  # get retuns the input matrix
+  get <- function() inputMatrix
+  
+  # setInverse sets the matrix inverse
+  setInverse <- function(inverse) matrixInverse <<- inverse
+  
+  # getInverse returns the matrix inverse
+  getInverse <- function() matrixInverse
+  
+  # Setup methods
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse);
+}
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## Returns any prievousally calculated inverse from inputMatrix
+##  or calculates, caches and returns the inverse.
+cacheSolve <- function(inputMatrix)
+{
+  # Check if the input has a cached inverse
+  inverse <- inputMatrix$getInverse();
+  
+  if(!is.null(inverse))
+  {
+    message("Getting cached data");
+    return(inverse);
+  }
+  
+  # Calculate the inverse
+  message("Calculating Inverse");
+  matrixData <-inputMatrix$get();
+  inverse <- solve(matrixData);
+  
+  # Cache and return the inverse
+  inputMatrix$setInverse(inverse);
+  
+  return(inverse);
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## ValidateMatrix is used to check that the matrix is square
+ValidateMatrix <- function(inputMatrix)
+{
+  nRow <- dim(inputMatrix)[1];
+  nCol <- dim(inputMatrix)[2];
+  
+  return(!is.null(nRow) && !is.null(nCol) && nRow == nCol);
 }
